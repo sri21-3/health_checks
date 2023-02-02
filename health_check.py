942376fa125f71e843dbd0f1457bb09da30c3453
sri21-3 <111290826+sri21-3@users.noreply.github.com>
2023-02-02 22:42:43 +0530

    updated with available memory function
    individual function messages are printed depending on output of functions

--- health_check.py
+++ health_check.py
@@ -6,14 +6,24 @@ def disk_usage(disk):
    free = du.free / du.total *100
    print("free:",free)
    return free > 20
-
+def available_memory_check():
+   available= psutil.virtual_memory().available
+   available_in_mb= available /1024 **2
+   return available_in_mb > 500
+   
 def cpu_percent_usage():
   pu = psutil.cpu_percent(1)
   print("cpu percentage for 1 sec:",pu)
-  return pu < 70
+  return pu < 80
 
-if not disk_usage("/") or not cpu_percent_usage():
-   print("ERROR!!")
+if not disk_usage("/"):
+   print("ERROR !!-DISK USAGE MORE THAN 80%")
+   
+elif not available_memory_check():
+   print("ERROR!!- Available memory is less than 500mb")
+
+elif not cpu_percent_usage():
+   print("ERROR!!-CPU USAGE morethan 80%")
 
 else:
    print("EVERY THING OK!!")
